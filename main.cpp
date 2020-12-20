4208e3c5538492568741463ebf5b2d1198fdfa0e
Benjamin Worthington <benworthin@icloud.com>
2020-12-20 00:25:20 -0600

    Basic main with user input and declaration of fibonacciSequence function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+using namespace std;
+
+void fibonacciSequence(int const &numbersToGenerate);
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int numbersToGenerate = 0;
+
+    cout << "The Fibonacci Sequence" << endl;
+    cout << "Enter how many Fibonacci numbers you'd like to generate: > ";
+    cin >> numbersToGenerate;
+
+    fibonacciSequence(numbersToGenerate);
+
     return 0;
+
+}
+
+void fibonacciSequence(int const &numbersToGenerate) {
+
+    
+
 }
